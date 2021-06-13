807f4f4ece16bb0be1be84466662e182ad2d2e7d
Greiciane93x <greiciane_araujo@hotmail.com>
2021-06-13 05:51:42 -0300

    Create Vetor.java

---
+++ Vetor.java
@@ -0,0 +1,22 @@
+package ed; 
+
+public class Vetor{ 
+  private Aluno[] alunos = new Aluno[100]; 
+  public void adiciona(Aluno aluno){
+  
+  }
+  public void pega(int posicao) {
+    return null; 
+  }
+  public boolean contain(Aluno aluno){
+    return false; 
+  }
+  public int tamanho(){
+    return 0; 
+  }
+  public String toString(){
+    return Arrays.toString(alunos); 
+  }
+  
+
+} 
