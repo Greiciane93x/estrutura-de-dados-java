42c0eee82cf3972bca4537ed75959be944a70bbb
Greiciane93x <greiciane_araujo@hotmail.com>
2021-06-13 06:09:03 -0300

    Update VetorTeste.java

--- VetorTeste.java
+++ VetorTeste.java
@@ -2,13 +2,16 @@ package ed;
 
 public class VetorTeste {
   public static void main(String[] args){
-    Aluno a1 = new Aluno("Ane");
-    Aluno a2 = new Aluno("Alice"); 
+     Aluno a1 = new Aluno("Ane");
+     Aluno a2 = new Aluno("Alice"); 
     
-   Vrtor lista =  new Vetor(); 
-   lista.adiciona(a1); 
-   lista.adiciona(a2); 
-    
-    System.out.println(lista); 
+     System.out.println(lista.tamanho()); 
+     Vetor lista =  new Vetor(); 
+     System.out.println(lista.tamanho()); 
+     lista.adiciona(a1); 
+     System.out.println(lista.tamanho()); 
+     lista.adiciona(a2); 
+
+     System.out.println(lista); 
   }  
 }
