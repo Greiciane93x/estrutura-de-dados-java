30866dadfe4c0c19fe3a295f415d32fa0d2ad497
Greiciane93x <greiciane_araujo@hotmail.com>
2021-06-13 05:59:08 -0300

    Update Vetor.java

--- Vetor.java
+++ Vetor.java
@@ -3,7 +3,12 @@ package ed;
 public class Vetor{ 
   private Aluno[] alunos = new Aluno[100]; 
   public void adiciona(Aluno aluno){
-  
+    for(int i = 0; i <= alunos.length; i++){
+      if(alunos[i] == null){
+        aluno[i] = aluno; 
+         break; 
+      }
+    }
   }
   public void pega(int posicao) {
     return null; 
